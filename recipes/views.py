dea507d745e9d7e8efe9c2bf3d2a9adfb231fdb9
Eduardo Prestes <dudu_insta@outlook.com>
2022-12-21 01:04:20 -0300

    update views, settings and urls

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Eduardo Prestes',
     })
+    
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Eduardo Prestes',
+    })
+
